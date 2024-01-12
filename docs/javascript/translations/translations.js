b587d3c22bfe3775cf03a9bae34cde5a0660efa2
Bruno Chiappetta <brunochiappetta26@gmail.com>
2024-01-12 11:20:03 -0300

    Docs v1.0.3 Automatic Translations based on nav language

--- docs/javascript/translations/translations.js
+++ docs/javascript/translations/translations.js
@@ -1,24 +1,40 @@
-/*==================== NAV BAR ====================*/
-const flagsElement = document.getElementById("flags")
-
-
+const flagsElement = document.getElementById("flags");
 const textsToChange = document.querySelectorAll("[data-section]");
 
-const changeLanguage = async language=>{
-    const requestJson = await fetch(`./javascript/translations/${language}.json`)
-    const texts = await requestJson.json()
-    // console.log(texts);
+const detectBrowserLanguage = () => {
+    const browserLanguage = navigator.language || navigator.userLanguage;
+    // El navegador devuelve el código de idioma en minúsculas, así que asegurémonos de que esté en minúsculas
+    return browserLanguage.toLowerCase();
+};
 
-    for(const textToChange of textsToChange){
-        const section = textToChange.dataset.section
-        const value = textToChange.dataset.value
+const changeLanguage = async (language) => {
+const requestJson = await fetch(`./javascript/translations/${language}.json`);
+const texts = await requestJson.json();
 
-        textToChange.innerHTML=texts[section][value];
+for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
 
-        // console.log( section, value)
-    }
+    textToChange.innerHTML = texts[section][value];
 }
+};
 
-flagsElement.addEventListener("click", (e)=>{
-    changeLanguage(e.target.dataset.language)
-})
+// Detectar el idioma del navegador al cargar la página y cambiar el idioma
+document.addEventListener("DOMContentLoaded", () => {
+    const browserLanguage = detectBrowserLanguage();
+
+  // Verificar si el idioma del navegador es compatible con los idiomas disponibles
+    const supportedLanguages = ["en", "es"];
+    const defaultLanguage = "en"; // Idioma por defecto si no se encuentra un idioma compatible
+
+    const initialLanguage = supportedLanguages.includes(browserLanguage)
+    ? browserLanguage
+    : defaultLanguage;
+
+  // Cambiar el idioma al cargar la página
+    changeLanguage(initialLanguage);
+});
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.dataset.language);
+});
