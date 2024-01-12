e007ee05f75aa346d22226c938f3fc22d7573b41
Bruno Chiappetta <brunochiappetta26@gmail.com>
2024-01-12 11:38:29 -0300

    Docs v1.0.4 minifier JS

--- docs/javascript/use-cases/textarea-height.js
+++
@@ -1,8 +0,0 @@
-/*===================== TEXT AREA ====================*/
-const textarea = document.getElementById('mensaje');
-
-textarea.addEventListener('input', function () {
-    this.style.height = 'auto';
-    this.style.height = (this.scrollHeight) + 'px';
-});
-
